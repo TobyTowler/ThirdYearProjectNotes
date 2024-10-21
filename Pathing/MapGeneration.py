6614bf7924cfa1bfe5baa98ebcd8e82e3f11880a
TobyTowler <tobytowler11@gmail.com>
2024-10-21 21:01:03 +0100

    map made

--- Pathing/MapGeneration.py
+++ Pathing/MapGeneration.py
@@ -3,10 +3,8 @@ import turtle
 import random
 
 
-mapBoundaries = []
-
-
 def createMap(n, k):
+    mapBoundaries = []
     # map points
     pointsarr = []
     for i in range(n):
@@ -24,40 +22,34 @@ def createMap(n, k):
             randY = random.randint(0, 100)
             holearr.append([randX, randY])
         mapBoundaries.append(holearr)
+    return mapBoundaries
 
 
 def sortPoints(arr):
-    start = arr[0][1]
-    newMap = [[]]
-    before = []
-    after = []
-    print(newMap, "\n")
-    for i in arr:
-        num = i[1]
-        if num < start:
-            before.append(arr[i])
-        else:
-            after.append(arr[i])
-
-    newMap += before
-    newMap.append(arr[0])
-    newMap += after
-    print("new map:::", newMap, "\n\n")
-    return newMap
-
-
-createMap(8, 2)
-mapBoundaries1 = sortPoints(mapBoundaries[0])
-print(mapBoundaries)
-
-print("\n\n\n NEWMAP", mapBoundaries1)
+    #    start = arr[0][1]
+    #    newMap = [[]]
+    #    before = []
+    #    after = []
+    #    print(newMap, "\n")
+    #    for i in arr:
+    #        num = i[1]
+    #        if num < start:
+    #            before.append(arr[i])
+    #        else:
+    #            after.append(arr[i])
+    #
+    #    newMap += before
+    #    newMap.append(arr[0])
+    #    newMap += after
+    #    print("new map:::", newMap, "\n\n")
+    #    return newMap
+    #
+    print("Hello")
 
 
+map = createMap(8, 2)
+print(map)
+# print(mapBoundaries)
+
+#
 turtle.pendown()
-# for i in range(len(mapBoundaries[0])):
-#     turtle.goto(mapBoundaries[0][i])
-
-
-for i in range(len(mapBoundaries1)):
-    turtle.goto(mapBoundaries1[i][0])
-turtle.exitonclick()
