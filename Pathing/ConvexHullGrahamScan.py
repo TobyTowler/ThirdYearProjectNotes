3cfd6472c23d9a1a8080ade4879a68077eb9a47f
TobyTowler <tobytowler11@gmail.com>
2024-11-06 22:16:35 +0000

    Pathing sorting angles

--- Pathing/ConvexHullGrahamScan.py
+++ Pathing/ConvexHullGrahamScan.py
@@ -1,4 +1,28 @@
 import random
+import math
+import turtle
+
+
+class Point:
+    X = -1
+    Y = -1
+    angle = -1
+
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
+        self.angle = -1
+
+    def __str__(self):
+        return f"Point(X: {self.X}, Y: {self.Y}, Angle: {self.angle})"
+
+
+def printPoints(points):
+    print("PRINTING POINTS")
+    for i in range(len(points) - 1):
+        print(points[i])
+
+    print("\n\n")
 
 
 def genPoints(num):
@@ -6,11 +30,44 @@ def genPoints(num):
     for i in range(num):
         randX = random.randint(0, 300)
         randY = random.randint(0, 300)
-        points.append([randX, randY, -1])  # X, Y, polar angle -1 is default
+        points.append(Point(randX, randY))
     return points
+
+
+def calcAngle(p1, p2):
+    return math.tan((p1.Y - p2.Y) - (p1.X - p2.X))
 
 
 def grahamScan(points):
     hull = []
-    points.sort(key=lambda x: x[1])
+    points.sort(key=lambda x: x.Y)
+    printPoints(points)
+
     hull.append(points[0])
+
+    origin = hull[0]
+    points.pop(0)
+    for i in range(len(points) - 2):
+        points[i].angle = calcAngle(origin, points[i])
+
+    points.sort(key=lambda x: x.angle)
+    printPoints(points)
+
+
+def drawPoints(arr):
+    turtle.penup()
+    for n in arr:
+        turtle.goto(n.X, n.Y)
+        turtle.pendown()
+
+    turtle.goto(arr[0].X, arr[0].Y)
+    turtle.exitonclick()
+
+
+def main():
+    map = genPoints(10)
+    grahamScan(map)
+    drawPoints(map)
+
+
+main()
